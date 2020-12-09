6bb1bfbc094e59556203c397482749e2bc862920
Micaela Vera <micavera2010@hotmail.com>
2020-12-09 16:56:31 -0300

    agrego cambios en CYK

--- src/main/java/gramatica/CYK.java
+++ src/main/java/gramatica/CYK.java
@@ -58,12 +58,11 @@ public class CYK {
 				}
 			}	
 						
-	//	    Para produccion con longitud 2: R_A -> R_B R_C. 
+	//	    Para cada produccion con longitud 2: R_A -> R_B R_C. 
 			for(int i = 1 ; i < n ; i++) {  
 				for (int j = 0; j < n - i ; j++) {
 					for(int k = 0 ; k < i ; k++) {
 						for (int p = 0 ; p < r ; p++) {
-	
 							Produccion produccion = gramatica.getProducciones().get(p);
 							if (produccion.getSimbolos().size() == 2) {
 								
