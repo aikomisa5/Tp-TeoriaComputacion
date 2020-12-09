c3349b85014ff54aa6c9ce6916f2a455a0db3685
Micaela Vera <micavera2010@hotmail.com>
2020-12-09 16:29:43 -0300

    agrego TestEjercicio1b en TestSuite

--- src/main/test/test/TestSuite.java
+++ src/main/test/test/TestSuite.java
@@ -5,6 +5,7 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({TestEjercicio1a_1.class,
+		TestEjercicio1b.class,
         TestEjercicio2a_1.class,
         TestEjercicio2a_2.class,
         TestEjercicio2b_1.class,
